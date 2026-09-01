9ceb45058aaaa8366b1d94ee6d8e3187ee26f8fb
balaji4621 <balaji123ssb@gmail.com>
2026-09-01 19:57:52 +0530

    feat: add loop animation wrapper for repeating sequences

---
+++ src/components/LoopAnimation.tsx
@@ -0,0 +1,33 @@
+﻿import React from 'react';
+import { AbsoluteFill, useCurrentFrame, useVideoConfig } from 'remotion';
+
+export interface LoopAnimationProps {
+  durationInFrames?: number;
+  children: React.ReactNode;
+}
+
+export const LoopAnimation: React.FC<LoopAnimationProps> = ({
+  durationInFrames = 60,
+  children,
+}) => {
+  const frame = useCurrentFrame();
+  const loopFrame = frame % durationInFrames;
+
+  return (
+    <AbsoluteFill
+      style={{
+        position: 'relative',
+        animation: 
+one,
+      }}
+    >
+      {React.Children.map(children, (child) =>
+        React.isValidElement(child)
+          ? React.cloneElement(child as React.ReactElement<any>, {
+              _loopFrame: loopFrame,
+            })
+          : child,
+      )}
+    </AbsoluteFill>
+  );
+};
